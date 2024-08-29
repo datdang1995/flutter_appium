f400c7b9d63e4bc7a24945920fabb4f4de24738b
DatDang <tddang@linagora.com>
2024-08-29 10:03:27 +0700

    Init Flutter Appium

---
+++ android/app/src/main/kotlin/com/datdang/flutter_appium/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.datdang.flutter_appium
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ integration_test/appium_test.dart
@@ -0,0 +1,6 @@
+import 'package:appium_flutter_server/appium_flutter_server.dart';
+import 'package:flutter_appium/main.dart';
+
+void main() {
+  initializeTest(app: const MyApp());
+}
